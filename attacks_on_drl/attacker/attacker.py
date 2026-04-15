0c3d87ad7f57af7b252e336e0e8d47b3d6affbfc
thomaoc1 <73828133+thomaoc1@users.noreply.github.com>
2026-04-15 20:12:50 +0200

    value function attacker

--- attacks_on_drl/attacker/attacker.py
+++ attacks_on_drl/attacker/attacker.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
 from stable_baselines3.common.vec_env.base_vec_env import VecEnvObs
 
+from attacks_on_drl.victim.victim import BaseVictim
+
 class BaseAttacker(ABC):
+    def __init__(self, victim: BaseVictim) -> None:
+        super().__init__()
+        self.victim = victim
+        
     @abstractmethod
     def step(self, observation: VecEnvObs) -> tuple[VecEnvObs, bool]:
         pass
---
+++ attacks_on_drl/attacker/value_function_attacker.py
@@ -0,0 +1,43 @@
+from typing import Iterator
+from stable_baselines3.common.vec_env.base_vec_env import VecEnvObs
+from torch import torch
+from torchattacks import FGSM
+
+from attacks_on_drl.attacker.attacker import BaseAttacker
+from attacks_on_drl.victim.victim import BaseVictim
+
+class VictimModuleWrapper(torch.nn.Module):
+    def __init__(self, victim_agent: BaseVictim):
+        super().__init__()
+        self.agent = victim_agent
+
+    def forward(self, obs):
+        return self.agent.get_action_logits(obs)
+
+    def parameters(self, recurse: bool = True) -> Iterator[torch.nn.Parameter]:
+        return self.agent.model_parameters()
+
+class ValueFunctionAttacker(BaseAttacker):
+    def __init__(self, victim: BaseVictim, attack_threshold: float, eps: float = 8 / 255) -> None:
+        super().__init__(
+            victim=victim
+        )
+        self.attack_threshold = attack_threshold
+        
+        wrapped_victim = VictimModuleWrapper(self.victim)
+        self._perturbation_method = FGSM(wrapped_victim, eps=eps)
+    
+    def _perturb_observation(self, observation: torch.Tensor) -> torch.Tensor:
+        return observation
+        
+    def step(self, observation: VecEnvObs) -> tuple[VecEnvObs, bool]:
+        value = self.victim.eval_state(observation).item()
+        is_attacked = value > self.attack_threshold
+        if is_attacked:
+            adversarial_observation = self._perturbation_method(observation).to_numpy()
+        else:
+            adversarial_observation = observation
+        
+        return adversarial_observation, is_attacked
+        
+        
