2e37b2887e2637535a99cd38f08dc904369a5239
thomaoc1 <73828133+thomaoc1@users.noreply.github.com>
2026-04-19 14:31:10 +0200

    Every n fgsm attacker

--- attacks_on_drl/attacker/__init__.py
+++ attacks_on_drl/attacker/__init__.py
@@ -1,5 +1,6 @@
 from .strategically_timed_attacker import StrategicallyTimedAttacker
 from .value_function_attacker import ValueFunctionAttacker
 from .fgsm_attacker import FGSMAttacker
+from .fgsm_every_n_attacker import FGSMEveryNAttacker
 
-__all__ = ["ValueFunctionAttacker", "StrategicallyTimedAttacker", "FGSMAttacker"]
+__all__ = ["ValueFunctionAttacker", "StrategicallyTimedAttacker", "FGSMAttacker", "FGSMEveryNAttacker"]
